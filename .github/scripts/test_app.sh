a90201bc93cf6fccb06f12d1bd238687e8e691fa
syedimranFB <syedimran@4blabs.com>
2023-05-26 13:45:37 +0530

    changed ios v again

--- .github/scripts/test_app.sh
+++ .github/scripts/test_app.sh
@@ -4,5 +4,5 @@ set -eo pipefail
 
 xcodebuild -workspace Calculator.xcworkspace \
             -scheme Calculator\ iOS \
-            -destination platform=iOS\ Simulator,OS=16.1,name=iPhone\ 14 \
+            -destination platform=iOS\ Simulator,OS=16.2,name=iPhone\ 14 \
             clean test | xcpretty
